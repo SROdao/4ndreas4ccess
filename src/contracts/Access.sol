1e6dfb43be6c9600ef8ed39a79c5dc9cb7b49dbc
chrisninetyone <chrisninetyone@gmail.com>
2021-10-23 17:56:13 -0400

    added array of ids and mapping

--- src/contracts/Access.sol
+++ src/contracts/Access.sol
@@ -1,8 +1,43 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract Access is ERC721 {
-    constructor() ERC721("4ccess", "4CCESS") public {
+contract Access is ERC721, ERC721Enumerable {
+    uint256[] public ids;
+    mapping(uint256 => bool) _idExists;
+
+    constructor() ERC721("4ccess", "4CCESS") {}
+
+    // TODO: restrict the ownership of this function to a minter/admin (openzeppelin)
+        // TODO: pass in a unique ID and add mapping (tokenUri, metadata)
+    function mint(uint256 _id) public {
+        //get unique id somehow
+
+        // require(!_colorExists[_color]) 
+        ids.push(_id);
+
+        // to address and token id passed in below
+        _safeMint(msg.sender, _id);
+        _idExists[_id] = true;
+    }
+
+
+    // Overriden methods implemented from ERC721Enumerable
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
