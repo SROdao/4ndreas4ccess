3a9ea052413024f3412018eb7d6fcaa5630d2382
chrisninetyone <chrisninetyone@gmail.com>
2021-10-23 15:10:38 -0400

    added specs

--- test/Access.test.js
+++ test/Access.test.js
@@ -1,3 +1,5 @@
+const { assert } = require('chai');
+
 const Access = artifacts.require('./Access.sol');
 
 require('chai')
@@ -5,12 +7,33 @@ require('chai')
     .should()
 
 contract('Access', (accounts) => {
+    let contract;
+
+    before(async () => {
+        contract = await Access.deployed()
+    })
+
     //test cases for 4ccess contract here
-    describe('deployment', async () => {
+    describe(`deployment`, async () => {
         //smoke test
         //check the address to make sure it got deployed on the network
-        it('deploys successfully', async() => {
-            
+        it(`deploys successfully`, async () => {
+            const address = contract.address;
+
+            assert.notEqual(address, 0x0)
+            assert.notEqual(address, '')
+            assert.notEqual(address, null)
+            assert.notEqual(address, undefined)
+        })
+
+        it(`has a name`, async () => {
+            const name = await contract.name()
+            assert.equal(name, '4ccess')
+        })
+
+        it(`has a symbol`, async () => {
+            const symbol = await contract.symbol()
+            assert.equal(symbol, '4CCESS')
         })
     })
 })
