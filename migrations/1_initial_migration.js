87bc4688462ecdd368c64c8587406559f2d897d0
chrisninetyone <chrisninetyone@gmail.com>
2021-10-23 15:10:52 -0400

    added a truffle migration

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 const Migrations = artifacts.require("Migrations");
 
+//create these migration files to put smart contracts on the blockchain
+//like a db migration in a web2.0 context
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
 };
